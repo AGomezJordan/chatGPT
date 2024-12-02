10f058b469416ced2bab36b3215da631d012e95d
Alvaro GOmez <alvarogomez.tb@gmail.com>
2024-12-02 13:21:15 +0100

    First promp to chatGPT

--- chatGPT-1.py
+++ chatGPT-1.py
@@ -1,6 +1,36 @@
 import os
+import json
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 APIKEY = os.getenv('OPENAI_API_KEY')
+client = OpenAI(
+    api_key = APIKEY
+)
+
+model = "gpt-3.5-turbo"
+prompt = "Elije un nombre para un elefante"
+messages = [
+    # {"role": "system", "content": "Eres un asistente creativo."},
+    {"role": "user", "content": prompt}
+]
+
+response = client.chat.completions.create(
+    model=model,
+    messages=messages,
+    n=3,
+    temperature=0.5,
+    max_tokens=20
+)
+
+print(response)
+print()
+
+# textGenerated = response.choices[0].message.content
+# print(textGenerated)
+
+for idx, choice in enumerate(response.choices):
+    textGenerated = choice.message.content
+    print(f"Respose {idx + 1}: {textGenerated}\n")
